83511ba2857976247f09d87c8e42d25550cff83d
Owen Synge <osynge@googlemail.com>
2021-03-31 08:53:20 +0200

    get test_runs

--- static/index.js
+++ static/index.js
@@ -10,6 +10,8 @@ const app = new Vue({
         'project_sk': 'xxx',
         'project_hn': '',
         'list_run_identifer': [],
+        'run_identifer_sk': 'xxx',
+        'list_test_run': [],
       }
     }
   },
@@ -26,6 +28,16 @@ const app = new Vue({
         }
       }
       this.getRunIdentifierList();
+    },
+    setRunIdentifierSk: function (sk) {
+      this.data.run_identifer_sk = sk;
+      this.getTestRunList();
+    },
+    async getTestRunList() {
+      const lurl = '/v1/test_run?run_identifer_sk=' + this.data.run_identifer_sk;
+      const res = await fetch(lurl);
+      const data = await res.json();
+      this.data.list_test_run = data;
     },
     async getRunIdentifierList() {
       const lurl = '/v1/run_identifer?project_sk=' + this.data.project_sk;
@@ -45,6 +57,7 @@ const app = new Vue({
       <project-picker :projects="data.list_project" @select-project="setProjectSk"></project-picker>
       You selected project {{ this.data.project_sk }} {{this.data.project_hn}}.
       list_run_identifer {{ this.data.list_run_identifer }}.
-      <run-identifer-picker :run_identifers="data.list_run_identifer"></run-identifer-picker>
+      <run-identifer-picker :run_identifers="data.list_run_identifer" @select-run-identifer="setRunIdentifierSk"></run-identifer-picker>
+      list_test_run {{ this.data.list_test_run }}.
     </div>`
 });
