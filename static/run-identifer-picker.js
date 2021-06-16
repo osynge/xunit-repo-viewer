52b370e89f96f81e0cc68a14e09f44bfda4b2660
Owen Synge <osynge@googlemail.com>
2021-06-16 09:26:56 +0200

    make pull down

--- static/run-identifer-picker.js
+++ static/run-identifer-picker.js
@@ -1,32 +1,24 @@
-Vue.component('run-identifer', {
-    props: ['sk', 'client_identifier', 'created'],
-    methods: {
-        selectRunIdentifer(item) {
-            this.$emit('select-run-identifer', item);
-        }
-    },
-    template: `
-    <div>
-        <button @click="selectRunIdentifer(sk)">
-        {{ client_identifier }}
-        </button>
-    </div>
-    `
-});
-
-
 Vue.component('run-identifer-picker', {
-    props: ['run_identifers'],
+    props: ['run_identifers'], data() {
+        return {
+            data: {
+                'run_identifer_selected': '',
+            }
+        }
+    },
     methods: {
-        selectRunIdentifer(e) {
-            this.$emit('select-run-identifer', e);
+        selectRunIdentifer() {
+            this.$emit('select-run-identifer', this.data.run_identifer_selected);
         }
     },
     template: `
     <div>
-          <li v-for="run_identifer in run_identifers">
-          <run-identifer :sk="run_identifer.sk" :client_identifier="run_identifer.client_identifier" :created="run_identifer.created" @select-run-identifer="selectRunIdentifer"></run-identifer>
-        </li>
+    <div>
+        Run Id:
+        <select v-model="data.run_identifer_selected" @change="selectRunIdentifer">
+            <option disabled value="">Please select</option>
+            <option v-for="run_identifer in run_identifers" v-bind:value="run_identifer.sk">{{run_identifer.client_identifier}}</option>
+        </select>
     </div>
     `
 });
